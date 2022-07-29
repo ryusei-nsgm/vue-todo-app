81651b2988e84488aa0c40da41f9623b8d1fd36a
Ryusei <sidapple@gmail.com>
2022-07-29 20:47:02 +0900

    init commit

---
+++ server/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Tutorial from '@/components/Tutorial'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Tutorial',
+      component: Tutorial
+    }
+  ]
+})
